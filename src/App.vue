fb5a7e390c2000e18bc28d3e9925ee40eabca562
Diego Reyes <diegorele13@gmail.com>
2022-06-11 09:41:12 -0500

    add: component header

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,12 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
-
 <template>
-  <p class="bg-red-600">holaaaaaaaaaaaa</p>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <the-header />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<script>
+import theHeader from "./components/global/theHeader.vue";
+export default {
+  components: {
+    theHeader,
+  },
+};
+</script>
