8dcb0878f661af77bbf1142c01d2d082a48a1fcd
Diego Reyes <diegorele13@gmail.com>
2022-06-11 09:40:17 -0500

    add: variables' colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      primaryRHA: "#183A37",
+      secondaryRHA: "#C44900",
+      tertiaryRHA: "#E86417",
+      backgroundRHA: "#DEE0E0",
+      whiteRHA: "#F0F2F2",
+      blackRHA: "#04151F",
+    },
     extend: {},
   },
   plugins: [],
