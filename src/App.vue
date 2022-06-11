47ab659272c998eb8b17943245459645fd5a47f9
Diego Reyes <diegorele13@gmail.com>
2022-06-11 14:43:16 -0500

    add: header

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script>
-import TheHeader from "./components/global/TheHeader.vue";
+import TheHeader from "./components/Global/TheHeader.vue";
 export default {
     components: {
         TheHeader,
