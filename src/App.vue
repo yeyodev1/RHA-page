6dc73018ea45a4f461b283d928215b022024db39
Diego Reyes <diegorele13@gmail.com>
2022-06-11 17:51:17 -0500

    nothing

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-    <the-header />
-    <the-hero />
+    <div class="background">
+        <the-header />
+        <the-hero />
+    </div>
 </template>
 
 <script>
@@ -14,6 +16,4 @@ export default {
 };
 </script>
 
-<style>
-@import url("https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap");
-</style>
+<style></style>
