6e8001cc4eb1e71788f852a920b184ae3a324ce3
Diego Reyes <diegorele13@gmail.com>
2022-06-11 16:51:09 -0500

    put: hero

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
     <the-header />
+    <the-hero />
 </template>
 
 <script>
 import TheHeader from "./components/Global/TheHeader.vue";
+import TheHero from "./components/Home/TheHero.vue";
 export default {
     components: {
         TheHeader,
+        TheHero,
     },
 };
 </script>
