fad2f0a0e9f844bac5d6448f1237ddc44fdff2bc
Diego Reyes <diegorele13@gmail.com>
2022-06-11 17:31:31 -0500

    put the correct fonts

---
+++ src/components/The/Services.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="w-full">
+        <!-- SERVICE DETAILS SECTION -->
+        <div class="w-2/5">
+            <p>{{ service }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  theme: {
-    colors: {
-      primaryRHA: "#183A37",
-      secondaryRHA: "#C44900",
-      tertiaryRHA: "#E86417",
-      backgroundRHA: "#DEE0E0",
-      whiteRHA: "#F0F2F2",
-      blackRHA: "#04151F",
+    content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+    theme: {
+        colors: {
+            primaryRHA: "#183A37",
+            secondaryRHA: "#C44900",
+            tertiaryRHA: "#E86417",
+            backgroundRHA: "#DEE0E0",
+            whiteRHA: "#F0F2F2",
+            blackRHA: "#04151F",
+        },
+        fontFamily: {
+            principal: ["Roboto", ...defaultTheme.fontFamily.sans],
+            secondary: ["Titillium Web"],
+        },
+        extend: {},
     },
-    extend: {},
-  },
-  plugins: [],
+    plugins: [],
 };
