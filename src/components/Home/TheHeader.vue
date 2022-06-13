aa3b13389c5e3a05e1442849732ebad371489930
Diego Reyes <diegorele13@gmail.com>
2022-06-13 18:41:15 -0500

    done: the header

--- src/components/Home/TheHeader.vue
+++ src/components/Home/TheHeader.vue
@@ -1,5 +1,7 @@
 <template>
-    <header class="h-16 w-full flex justify-between">
+    <header
+        class="h-16 w-full flex justify-between max-w-7xl mx-auto hover:bg-whiteRHA"
+    >
         <figure class="w-16 h-9 flex justify-center relative top-5 left-10">
             <img
                 src="../../assets/logo.png"
@@ -18,15 +20,35 @@
             </button>
         </div>
         <div
-            class="bg-secondaryRHA w-36 h-11 relative rounded-xl top-4 right-10"
+            class="bg-secondaryRHA hover:bg-tertiaryRHA w-36 h-11 relative rounded-xl top-4 right-10"
         >
-            <button class="w-full h-11">Cotizar</button>
+            <button
+                class="w-full h-11 flex justify-center items-center text-whiteRHA"
+                v-for="(icon, index) in icons"
+                :key="index"
+            >
+                Cotizar
+                <Icons
+                    :name="icon.name"
+                    class="w-4 h-4 hover:w-8 hover:h-6 text-whiteRHA"
+                />
+            </button>
         </div>
     </header>
 </template>
 
 <script>
-export default {};
+import Icons from "../Global/Icons.vue";
+export default {
+    components: { Icons },
+    data: () => ({
+        icons: [
+            {
+                name: "arrowRight",
+            },
+        ],
+    }),
+};
 </script>
 
 <style></style>
