cbf311a73042ce799e9a6a7db2ccc0758cbda7a9
Diego Reyes <diegorele13@gmail.com>
2022-06-13 14:35:04 -0500

    missing: icon of the button

--- src/components/Home/TheHero.vue
+++ src/components/Home/TheHero.vue
@@ -1,9 +1,38 @@
 <template>
-    <div class="font-secondary">hgoolaaaa</div>
+    <div class="font-secondary">
+        <div
+            class="w-full bg-primaryRHA h-20 flex justify-center items-center"
+            v-for="(service, index) in services"
+            :key="index"
+        >
+            <the-services :service="service.service"> </the-services>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {};
+import TheServices from "../The/Services.vue";
+export default {
+    components: {
+        TheServices,
+    },
+    data: () => ({
+        services: [
+            {
+                service: "boletines de senae",
+            },
+            {
+                service: "integracion tecnólogica de servicios",
+            },
+            {
+                service: "asesoria personalizada",
+            },
+            {
+                service: "elaboracion de consultas de aduana",
+            },
+        ],
+    }),
+};
 </script>
 
 <style></style>
