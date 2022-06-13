ed0b9f113e5780e9580c1bccaddbafa412a10bf4
Diego Reyes <diegorele13@gmail.com>
2022-06-13 15:13:52 -0500

    renamed: position

---
+++ src/components/Home/TheHeader.vue
@@ -0,0 +1,32 @@
+<template>
+    <header class="h-16 w-full flex justify-between">
+        <figure class="w-16 h-9 flex justify-center relative top-5 left-10">
+            <img
+                src="../../assets/logo.png"
+                alt="logo de reyes, hijos y asociados"
+            />
+        </figure>
+        <div class="flex w-1/4 justify-between">
+            <button>
+                <p class="text-blackRHA">Nosotros</p>
+            </button>
+            <button>
+                <p class="text-blackRHA">Servicios</p>
+            </button>
+            <button>
+                <p class="text-blackRHA">Contacto</p>
+            </button>
+        </div>
+        <div
+            class="bg-secondaryRHA w-36 h-11 relative rounded-xl top-4 right-10"
+        >
+            <button class="w-full h-11">Cotizar</button>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
