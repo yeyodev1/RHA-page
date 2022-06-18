e0986328bfc9db8090d4c7690bba8d7bf40ea168
Diego Reyes <diegorele13@gmail.com>
2022-06-17 23:21:07 -0500

    missing: finish the hero

--- src/components/Home/TheHero.vue
+++ src/components/Home/TheHero.vue
@@ -1,12 +1,32 @@
 <template>
-    <div class="font-secondary">
-        <div
-            class="w-full h-20 flex justify-center items-center"
-            v-for="(service, index) in services"
-            :key="index"
-        >
-            <the-services :service="service.service" :id="service.id" class="">
-            </the-services>
+    <div class="pt-96 pb-72">
+        <div class="font-secondary max-w-7xl w-full mx-auto px-10 py-5 slider">
+            <div
+                class="w-full h-20 flex justify-center items-center"
+                v-for="(service, index) in services"
+                :key="index"
+            >
+                <the-services
+                    :service="service.service"
+                    :id="service.id"
+                    class=""
+                >
+                </the-services>
+            </div>
+        </div>
+        <div class="menu w-2/5 px-10 py-5">
+            <li>
+                <a href="#item-1" class="w-6">1</a>
+            </li>
+            <li>
+                <a href="#item-2">2</a>
+            </li>
+            <li>
+                <a href="#item-3">3</a>
+            </li>
+            <li>
+                <a href="#item-4">4</a>
+            </li>
         </div>
     </div>
 </template>
@@ -40,4 +60,26 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+div,
+li {
+    list-style: none;
+}
+div.slider {
+    position: relative;
+}
+div.slider div {
+    position: absolute;
+    opacity: 0;
+    width: inherit;
+    height: inherit;
+    transition: opacity 0.5s;
+}
+div.slider div:first-child {
+    opacity: 1;
+}
+
+div.slider div the-services:target {
+    opacity: 1;
+}
+</style>
