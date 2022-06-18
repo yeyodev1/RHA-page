b768021626dcfe8e89fa1a99fcd2eb962c6022ff
Diego Reyes <diegorele13@gmail.com>
2022-06-17 23:19:17 -0500

    add: background image with a gradient

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,16 @@ export default {
 
 <style scoped>
 #background {
-    background-image: url("./assets/Images/background.png");
+    background: linear-gradient(
+            150.52deg,
+            #04151f 16.66%,
+            rgba(4, 21, 31, 0.526586) 58.47%,
+            rgba(4, 21, 31, 0.191211) 73.65%,
+            rgba(4, 21, 31, 0) 91.09%
+        ),
+        url("./assets/Images/background.png");
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
 }
 </style>
