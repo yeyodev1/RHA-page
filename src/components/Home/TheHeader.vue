2d246cca48ada12bd6da740a660392de7e5c0a02
Diego Reyes <diegorele13@gmail.com>
2022-06-20 16:13:34 -0500

    done: headers

--- src/components/Home/TheHeader.vue
+++ src/components/Home/TheHeader.vue
@@ -6,12 +6,27 @@
         >
             <!-- SPACE WHERE IS THE LOGO -->
             <div class="w-6 h-4 flex justify-center small-logo"></div>
-            <!-- BUTTON -->
+            <!-- SPACE WHERES THE BUTTON -->
             <button class="">
                 <div class="flex w-5 h-5 flex-col">
                     <div class="w-full h-5 border-b border-whiteRHA"></div>
                     <div class="w-full h-5 border-b border-whiteRHA"></div>
                     <div class="w-full h-5 border-b border-whiteRHA"></div>
+                </div>
+            </button>
+        </div>
+        <!-- HEADER IS SM ADVICES -->
+        <div
+            class="w-full h-28 hidden lg:hidden sm:flex items-center mx-10 justify-between"
+        >
+            <!-- SPACE WHERE IS THE LOGO -->
+            <div class="w-16 h-8 small-logo"></div>
+            <!-- SPACE WHERES THE BUTTON -->
+            <button class="">
+                <div class="flex w-9 h-7 flex-col">
+                    <div class="w-full h-7 border-b border-whiteRHA"></div>
+                    <div class="w-full h-7 border-b border-whiteRHA"></div>
+                    <div class="w-full h-7 border-b border-whiteRHA"></div>
                 </div>
             </button>
         </div>
