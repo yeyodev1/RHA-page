644526b3bdf11eb48c7d35778bd10eac806a500a
Diego Reyes <diegorele13@gmail.com>
2022-06-22 20:55:04 -0500

    add: OurServices

---
+++ src/components/Home/OurServices.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="w-full flex justify-center">
+        <div class="w-4/5 mt-44 flex flex-col justify-center">
+            <div class="flex justify-center">
+                <h2
+                    class="border-l-2 border-secondaryRHA pl-3 text-2xl sm:text-4xl"
+                >
+                    Nuestros Servicios
+                </h2>
+            </div>
+            <div class="w-full mt-16">
+                <p class="text-sm sm:text-3xl text-center">
+                    Siempre aseguramos la satisfacción del cliente con una
+                    atención de calidad, siendo eficientes y transparentes en
+                    todo lo relacionado a los procesos que conllevan la
+                    actividad de comercio exterior
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
