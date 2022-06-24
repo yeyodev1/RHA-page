34f1db0490719e74969a4f12e8f4c970d17980fa
Diego Reyes <diegorele13@gmail.com>
2022-06-23 21:52:12 -0500

    fix: styles

--- src/components/Home/OurServices.vue
+++ src/components/Home/OurServices.vue
@@ -20,7 +20,7 @@
         </div>
         <!-- CARDS WITH THE SERVICES -->
         <div
-            class="mt-12 flex flex-col sm:flex-row sm:w-full overflow-scroll justify-center sm:items-start sm:justify-start md:justify-center"
+            class="mt-12 flex flex-col sm:flex-row sm:w-full overflow-scroll lg:overflow-hidden justify-center sm:items-start sm:justify-start md:justify-center"
         >
             <div
                 class="sm:flex"
