df75f8461f013f57a6e8403ec8d6596f4a91c383
Diego Reyes <diegorele13@gmail.com>
2022-06-29 17:34:08 -0500

    add: vue router

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/index.css";
 import store from "../store/Index";
+import router from "../Router/index";
 
 const app = createApp(App);
 
+// STORE
 app.use(store);
+// ROUTER
+app.use(router);
 
 app.mount("#app");
