8a810377458af50b86898b7282fd03496abfde52
Diego Reyes <diegorele13@gmail.com>
2022-06-28 19:47:08 -0500

    import store

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/index.css";
+import store from "../store/Index";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+app.use(store);
+
+app.mount("#app");
