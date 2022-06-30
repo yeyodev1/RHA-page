15320ff4a0191919c86de583e0f79bcb94812768
Diego Reyes <diegorele13@gmail.com>
2022-06-30 16:29:01 -0500

    add: home view

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-    <home />
+    <main id="app" class="w-screen h-screen">
+        <router-view></router-view>
+    </main>
 </template>
 
 <script>
-import home from "./Pages/Home/Index.vue";
+import home from "./views/Home.vue";
 
 export default {
     components: {
