41f5e0362b5c11f8e74d7435b89e2643fed59f7d
Diego Reyes <diegorele13@gmail.com>
2022-06-30 16:22:45 -0500

    relocated: router

---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+
+// COMPONENTS
+import Home from "../views/Home.vue";
+import services from "../views/Services.vue";
+
+const routes = [
+    {
+        path: "/",
+        name: "home",
+        component: Home,
+    },
+    {
+        path: "/servicios",
+        name: "servicios",
+        component: services,
+    },
+];
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+});
+
+export default router;
