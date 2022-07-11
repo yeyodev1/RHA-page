3926a3822fb7daedc4ffa548d81587f7646c0f1b
Diego Reyes <diegorele13@gmail.com>
2022-07-11 16:59:36 -0500

    created: services hero

---
+++ src/components/Services/theHero.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="w-full flex flex-col justify-center items-center">
+        <div class="w-3/4 mt-44 mb-4">
+            <h4 class="text-3xl font-principal sm:text-5xl text-whiteRHA">
+                Nuestros servicios
+            </h4>
+        </div>
+        <div class="w-3/4 mb-20">
+            <p class="font-principal text-base sm:text-3xl text-whiteRHA">
+                Asesoramiento en aplicación de partidas arancelarias, consultas
+                legales y de aforo para todos nuestros clientes
+            </p>
+        </div>
+    </div>
+</template>
