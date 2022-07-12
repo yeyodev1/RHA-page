c67bff09d8283bcf2e5397623a17432210f00365
Diego Reyes <diegorele13@gmail.com>
2022-07-11 19:16:19 -0500

    add: work component

---
+++ src/components/Services/worksDetails.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="w-full">
+        <div v-for="(work, index) in works" :key="index">
+            <works
+                :image="work.image"
+                :name="work.name"
+                :description="work.description"
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+import works from "./Works/works.vue";
+
+export default {
+    components: {
+        works,
+    },
+    data: () => ({
+        works: [
+            {
+                image: "../../../images/image-1.png",
+                name: "lorem ipsum ",
+                description:
+                    "lorem ipsum lorem ipsummmajs faster herbsum ahdb ridsji sdfaisrohfk ribeye",
+            },
+            {
+                image: "../../../images/image-2.png",
+                name: "lorem ipsum ",
+                description:
+                    "lorem ipsum lorem ipsummmajs faster herbsum ahdb ridsji sdfaisrohfk ribeye",
+            },
+            {
+                image: "../../../images/image-3.png",
+                name: "lorem ipsum ",
+                description:
+                    "lorem ipsum lorem ipsummmajs faster herbsum ahdb ridsji sdfaisrohfk ribeye",
+            },
+        ],
+    }),
+};
+</script>
