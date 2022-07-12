b658cca01eefdb128f44325b5accfd5dcb14313c
Diego Reyes <diegorele13@gmail.com>
2022-07-12 15:32:27 -0500

    added: contact component

--- src/views/Services.vue
+++ src/views/Services.vue
@@ -3,15 +3,23 @@
         <the-header />
         <the-hero />
     </div>
+    <div id="background-2">
+        <works-details />
+    </div>
+    <contact />
 </template>
 
 <script>
+import contact from "../components/Home/Contact.vue";
+import worksDetails from "../components/Services/worksDetails.vue";
 import TheHeader from "../components/Home/TheHeader.vue";
 import TheHero from "../components/Services/theHero.vue";
 export default {
     components: {
+        contact,
         TheHeader,
         TheHero,
+        worksDetails,
     },
 };
 </script>
@@ -26,6 +34,13 @@ export default {
             rgba(4, 21, 31, 0) 91.09%
         ),
         url("../static/Services/backgroundServices-1.jpg");
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+}
+
+#background-2 {
+    background: url("../static/Services/backgroundServices-2.jpg") fixed 50% 50%;
     background-repeat: no-repeat;
     background-position: center;
     background-size: cover;
