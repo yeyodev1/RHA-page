e261db6dec923185e0b514189c9f0339cf14841f
Diego Reyes <diegorele13@gmail.com>
2022-07-13 16:12:40 -0500

    fix: form

--- src/components/Card/Form.vue
+++ src/components/Card/Form.vue
@@ -4,7 +4,7 @@
         class="w-full bg-grayRHA bg-bottom flex justify-center items-center"
     >
         <div
-            class="w-4/5 bg-whiteRHA rounded-xl pt-5 relative -top-16 sm:ml-36 lg:w-2/3 lg:-top-24 lg:left-12"
+            class="max-w-7xl w-4/5 bg-whiteRHA rounded-xl pt-5 relative -top-16 sm:ml-36 lg:w-2/3 lg:-top-24 lg:left-12"
         >
             <h2 class="text-xs sm:text-2xl ml-6 sm:ml-12 sm:mb-8 mb-4 lg:mb-8">
                 Empecemos a charlar
