2bc1d62525d76833c8f697c024cf67f10523b6a4
Diego Reyes <diegorele13@gmail.com>
2023-03-07 17:58:20 -0500

    styles

--- src/components/Services/worksDetails.vue
+++ src/components/Services/worksDetails.vue
@@ -1,115 +1,113 @@
 <template>
-    <div class="w-full mx-auto">
-        <div
-            class="w-full pb-10 flex flex-col sm:flex-row sm:px-11 max-w-6xl mx-auto"
-        >
-            <div
-                class="w-4/5 mx-auto pt-8 sm:pl-9 flex flex-col justify-center max-w-6xl"
-            >
-                <div class="pt-5">
-                    <h4
-                        class="text-blackRHA font-principal font-semibold text-base sm:text-3xl lg:"
-                    >
-                        lorem ipsum
-                    </h4>
-                </div>
-                <div class="pb-3">
-                    <p class="font-principal text-blackRHA text-base">
-                        "Asesoramiento en aplicación de partidas arancelarias,
-                        consultas legales y de aforo para todos nuestros
-                        clientes. Asesoramiento en aplicación de partidas
-                        arancelarias, consultas legales y de aforo para todos
-                        nuestros clientes."
-                    </p>
-                </div>
-            </div>
-            <div class="w-full flex justify-end max-w-6xl mx-auto">
-                <img
-                    class="image-width w-full h-36 object-cover no-reped"
-                    src="../../../images/image-1.jpg "
-                    alt=""
-                />
-            </div>
+  <div class="w-full mx-auto py-16">
+    <div
+      class="w-full pb-10 flex flex-col sm:flex-row sm:px-11 max-w-6xl mx-auto"
+    >
+      <div
+        class="w-4/5 mx-auto pt-8 sm:px-9 flex flex-col justify-center max-w-6xl backdrop-blur-md rounded-xl"
+      >
+        <div class="pt-5 mb-3">
+          <h4
+            class="text-blackRHA font-principal font-semibold text-base sm:text-3xl lg:"
+          >
+            Procesos de Importación y Exportación
+          </h4>
         </div>
-        <div
-            class="w-full sm:pt-2 pb-10 flex flex-col sm:flex-row-reverse sm:pr-8 sm:pl-24 max-w-6xl mx-auto"
-        >
-            <div
-                class="w-4/5 mx-auto pt-8 sm:pl-9 flex flex-col justify-center"
-            >
-                <div class="pt-5">
-                    <h4
-                        class="text-blackRHA font-principal font-semibold text-base sm:text-3xl lg:"
-                    >
-                        lorem ipsum
-                    </h4>
-                </div>
-                <div class="pb-3">
-                    <p class="font-principal text-blackRHA text-base">
-                        "Asesoramiento en aplicación de partidas arancelarias,
-                        consultas legales y de aforo para todos nuestros
-                        clientes. Asesoramiento en aplicación de partidas
-                        arancelarias, consultas legales y de aforo para todos
-                        nuestros clientes."
-                    </p>
-                </div>
-            </div>
-            <div class="w-full flex justify-start">
-                <img
-                    class="image-width w-full h-36 object-cover no-reped"
-                    src="../../../images/image-2.jpg "
-                    alt=""
-                />
-            </div>
+        <div class="pb-3">
+          <p class="font-principal text-blackRHA text-base">
+            Ofrecemos servicios de gestión de importaciones a consumo de
+            mercancías extranjeras, asegurándonos de cumplir con todas las
+            formalidades y obligaciones aduaneras para garantizar la libre
+            circulación de los productos. También brindamos asesoramiento en
+            trámites de exportación a consumo para la salida definitiva de
+            bienes del país, cumpliendo con todas las formalidades y
+            obligaciones aduaneras necesarias.
+          </p>
         </div>
-        <div
-            class="w-full pb-10 flex flex-col sm:flex-row sm:px-11 max-w-6xl mx-auto"
-        >
-            <div
-                class="w-4/5 mx-auto pt-8 sm:pl-9 flex flex-col justify-center"
-            >
-                <div class="pt-5">
-                    <h4
-                        class="text-blackRHA font-principal font-semibold text-base sm:text-3xl"
-                    >
-                        lorem ipsum
-                    </h4>
-                </div>
-                <div class="pb-3">
-                    <p class="font-principal text-blackRHA text-base">
-                        Asesoramiento en aplicación de partidas arancelarias,
-                        consultas legales y de aforo para todos nuestros
-                        clientes. Asesoramiento en aplicación de partidas
-                        arancelarias, consultas legales y de aforo para todos
-                        nuestros clientes.
-                    </p>
-                </div>
-            </div>
-            <div class="w-full flex justify-end">
-                <img
-                    class="image-width w-full h-36 object-cover"
-                    src="../../../images/image-3.jpg "
-                    alt=""
-                />
-            </div>
-        </div>
+      </div>
+      <div class="w-full flex justify-end max-w-6xl mx-auto mt-5 sm:mt-0">
+        <img
+          class="rounded-xl image-width w-full h-36 object-cover no-reped bg-origin-padding"
+          src="../../../images/image-1.jpg "
+          alt=""
+        />
+      </div>
     </div>
+    <div
+      class="w-full sm:pt-2 pb-10 flex flex-col sm:flex-row-reverse sm:pr-8 sm:pl-24 max-w-6xl mx-auto"
+    >
+      <div
+        class="w-4/5 mx-auto pt-8 sm:px-9 flex flex-col justify-center backdrop-blur-md rounded-xl"
+      >
+        <div class="pt-5 mb-3">
+          <h4
+            class="text-blackRHA font-principal font-semibold text-base sm:text-3xl lg:"
+          >
+            Trámites aduaneros
+          </h4>
+        </div>
+        <div class="pb-3">
+          <p class="font-principal text-blackRHA text-base">
+            "Asesoramiento en aplicación de partidas arancelarias, consultas
+            legales y de aforo para todos nuestros clientes. Asesoramiento en
+            aplicación de partidas arancelarias, consultas legales y de aforo
+            para todos nuestros clientes."
+          </p>
+        </div>
+      </div>
+      <div class="w-full flex justify-start">
+        <img
+          class="image-width w-full h-36 object-cover no-reped rounded-xl"
+          src="../../../images/image-2.jpg "
+          alt=""
+        />
+      </div>
+    </div>
+    <div
+      class="w-full pb-10 sm:pb-0 flex flex-col sm:flex-row sm:px-11 max-w-6xl mx-auto backdrop-blur-md"
+    >
+      <div class="w-4/5 mx-auto pt-8 sm:px-9 flex flex-col justify-center">
+        <div class="pt-5 mb-3">
+          <h4
+            class="text-blackRHA font-principal font-semibold text-base sm:text-3xl"
+          >
+            Logística Local
+          </h4>
+        </div>
+        <div class="pb-3">
+          <p class="font-principal text-blackRHA text-base">
+            Asesoramiento en aplicación de partidas arancelarias, consultas
+            legales y de aforo para todos nuestros clientes. Asesoramiento en
+            aplicación de partidas arancelarias, consultas legales y de aforo
+            para todos nuestros clientes.
+          </p>
+        </div>
+      </div>
+      <div class="w-full flex justify-end">
+        <img
+          class="image-width w-full h-36 object-cover rounded-xl"
+          src="../../../images/image-3.jpg "
+          alt=""
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
 .image-width {
-    width: 300px;
+  width: 300px;
 }
-@media only screen and (min-device-width: 768px) and (max-device-width: 1024px) {
-    .image-width {
-        width: 14.5rem;
-        height: 26.563rem;
-    }
+@media only screen and (min-device-width: 640px) and (max-device-width: 822px) {
+  .image-width {
+    width: 14.5rem;
+    height: 26.563rem;
+  }
 }
-@media only screen and (min-device-width: 1024px) and (max-device-width: 1280px) {
-    .image-width {
-        width: 25.75rem;
-        height: 25.938rem;
-    }
+@media only screen and (min-device-width: 823px) {
+  .image-width {
+    width: 25.75rem;
+    height: 25.938rem;
+  }
 }
 </style>
