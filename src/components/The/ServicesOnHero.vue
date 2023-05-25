35ba61bcbcff086a849fe2ba51260aaf69505e86
Diego Reyes <diegorele13@gmail.com>
2023-05-25 13:34:19 -0500

    fix: styles unnecesary

--- src/components/The/ServicesOnHero.vue
+++ src/components/The/ServicesOnHero.vue
@@ -1,31 +1,31 @@
 <template>
-    <div class="w-full">
-        <!-- SERVICE DETAILS SECTION -->
-        <div :id="getId" class="w-4/5 lg:w-1/3 lg:px-16">
-            <p class="text-3xl sm:text-5xl text-whiteRHA font-principal">
-                {{ service }}
-            </p>
-        </div>
+  <div class="w-full">
+    <!-- SERVICE DETAILS SECTION -->
+    <div :id="getId" class="">
+      <p class="text-3xl sm:text-5xl text-whiteRHA font-principal">
+        {{ service }}
+      </p>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    //PROPS TO WRITE THE SERVICES THAT ARE PROVIDED
-    props: {
-        service: {
-            type: String,
-            required: true,
-        },
-        id: {
-            type: String,
-            required: true,
-        },
+  //PROPS TO WRITE THE SERVICES THAT ARE PROVIDED
+  props: {
+    service: {
+      type: String,
+      required: true,
     },
-    computed: {
-        getId() {
-            return this.id;
-        },
+    id: {
+      type: String,
+      required: true,
     },
+  },
+  computed: {
+    getId() {
+      return this.id;
+    },
+  },
 };
 </script>
